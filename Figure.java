efa88fc915400342ee64b1ebd05c625d65cf0843
iimnotroot <davidmorenofeernandez@gmail.com>
2026-02-12 18:14:43 +0100

    Version 0 Figure.java

---
+++ Figure.java
@@ -0,0 +1,13 @@
+public abstract class Figure {
+
+    /**
+     * Returns the main Point of the SimpleFigure
+     */
+
+    public abstract Point pos();
+
+    public abstract void move(int dx, int dy);
+
+    public abstract String toString();
+
+}
