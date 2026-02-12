af66b67bae84a495a2439306fe68f33130e4b164
iimnotroot <davidmorenofeernandez@gmail.com>
2026-02-12 18:15:19 +0100

    Version 0 Point.java

---
+++ Point.java
@@ -0,0 +1,22 @@
+public class Point {
+    public int x;
+    public int y;
+
+    /**
+     * moves point adding delta x and delta y
+     */
+
+    public Point(int ux, int uy) {
+        x = ux;
+        y = uy;
+    }
+
+    public void move(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public String toString() {
+        return "["+x+", "+y+"]";
+    }
+}
