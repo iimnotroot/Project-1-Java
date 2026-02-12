497fb963a8c08325be52c27b3f2d9c4c2a5b410c
iimnotroot <davidmorenofeernandez@gmail.com>
2026-02-12 18:16:30 +0100

    Version 0 Rect.java

---
+++ Rect.java
@@ -0,0 +1,21 @@
+public final class Rect extends SimpleFigure{
+    public int sizex;
+    public int sizey;
+
+    /**
+     * Creates a Rectangle with a min Point and max Point
+     */
+
+    public Rect(Point pmin, Point pmax) {
+        super(pmin);
+        sizex = pmax.x - pmin.x;
+        sizey = pmax.y - pmin.y;
+    }
+
+    @Override
+    public String toString() {
+        Point max = new Point(pos.x, pos.y);
+        max.move(sizex,sizey);
+        return " Rect: [" + pos + "," + max + "]";
+    }
+}
